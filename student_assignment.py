97e85e9c28b68f739314e71b5c939b520689bc49
JeffreyCC <jeffrey_cc_chen@asus.com>
2025-02-04 10:49:14 +0800

    hw02_1

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,19 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+  # load pdf
+  loader = PyPDFLoader(q1_pdf)
+  documents = loader.load()
+  print("Pages in the original document: ", len(documents))
+
+  # CharacterTextSplitter
+  text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+  docs = text_splitter.split_documents(documents)
+  page_count = len(docs)
+  print("Length of chunks after splitting pages: ", len(docs))
+  print("the last page", docs[page_count-1])
+
+  return docs
 
 def hw02_2(q2_pdf):
     pass
